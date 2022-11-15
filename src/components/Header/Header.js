ea6b1ac017fba862ff7999f67f38a36703794a23
JubayerDev <ajashes858@gmail.com>
2022-11-15 20:17:03 +0600

    header added and responsive

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,27 @@
-import React from 'react';
-import './Header.css';
+import React from "react";
+import logo from "../../images/Logo.svg";
+import "./Header.css";
 
 const Header = () => {
-    return (
-        <div>
-            
+  return (
+    <nav className="header">
+      <a href="/">
+        <img src={logo} alt="" />
+      </a>
+      <div>
+        <input type="checkbox" id="checkbox_toggle" />
+        <label htmlFor="checkbox_toggle" className="hamburger">
+          &#9776;
+        </label>
+        <div className="menu">
+          <a href="/shop">Shop</a>
+          <a href="/orders">Orders</a>
+          <a href="/inventory">Inventory</a>
+          <a href="/about">About</a>
         </div>
-    );
+      </div>
+    </nav>
+  );
 };
 
 export default Header;
