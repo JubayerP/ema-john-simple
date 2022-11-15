6f97c54fef7a364fd5c3b2cce2523a24ad6a3b6b
JubayerDev <ajashes858@gmail.com>
2022-11-15 22:20:23 +0600

    products styled

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './Product.css';
+import {BsCartPlusFill} from 'react-icons/bs'
 
 const Product = ({ product }) => {
     const { name, img, seller, price, ratings } = product;
@@ -13,7 +14,7 @@ const Product = ({ product }) => {
                 <p className='product-seller'><small>Seller: {seller}</small></p>
                 <p className='product-ratings'><small>Ratings: { ratings} Starts</small></p>
             </div>
-            <button className='btn-cart'><p>Add to Cart</p></button>
+            <button className='btn-cart flex'><p>Add to Cart</p> <BsCartPlusFill /> </button>
         </div>
     );
 };
