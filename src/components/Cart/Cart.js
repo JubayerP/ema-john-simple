3c509bc2c5ad4fe02783abdcb94ed0461d0df2f5
JubayerDev <ajashes858@gmail.com>
2022-11-16 17:32:20 +0600

    integrate cart calculation with localstorage

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -2,23 +2,25 @@ import React from "react";
 import "./Cart.css";
 
 const Cart = ({ cart }) => {
-    let total = 0;
-    let shipping = 0;
-    for (const product of cart) {
-        total = total + product.price;
-        shipping = shipping + product.shipping;
-    }
-    const tax = parseFloat((total * 0.1).toFixed(2));
-    const grandTotal = (total + shipping + tax).toFixed(2)
+  let total = 0;
+  let shipping = 0;
+  let quantity = 0;
+  for (const product of cart) {
+    quantity = quantity + product.quantity;
+    total = total + product.price * product.quantity;
+    shipping = shipping + product.shipping;
+  }
+  const tax = parseFloat((total * 0.1).toFixed(2));
+  const grandTotal = (total + shipping + tax).toFixed(2);
   return (
     <div className="cart">
       <h4 className="order-summary">Order Summary</h4>
       <div className="cart-info">
-              <p className="selected-items">Selected Items: {cart.length}</p>
-              <p>Total Price: ${ total}</p>
-              <p>Total Shipping Charge: ${ shipping}</p>
-              <p>Tax: ${ tax}</p>
-              <h6>Grand Total: ${ grandTotal}</h6>
+        <p className="selected-items">Selected Items: {quantity}</p>
+        <p>Total Price: ${total}</p>
+        <p>Total Shipping Charge: ${shipping}</p>
+        <p>Tax: ${tax}</p>
+        <h6>Grand Total: ${grandTotal}</h6>
       </div>
     </div>
   );
