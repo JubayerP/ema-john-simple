eafd02d683d67be6fc2b35ea674688d6e331243e
JubayerDev <ajashes858@gmail.com>
2022-11-19 03:30:35 +0600

    setup react router dom

--- src/App.js
+++ src/App.js
@@ -1,11 +1,39 @@
-import Header from "./components/Header/Header";
-import Shop from "./components/Shop/Shop";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import About from "./components/About/About";
+import Main from "./layouts/Main";
+import Shop from './components/Shop/Shop';
+import Orders from './components/Orders/Orders';
+import Inventory from './components/Inventory/Inventory'
+
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <Main />,
+      children: [
+        {
+          path: '/',
+          element: <Shop />
+        },
+        {
+          path: 'orders',
+          element: <Orders />
+        },
+        {
+          path: 'inventory',
+          element: <Inventory />
+        }
+      ]
+    },
+    {
+      path: 'about',
+      element: <About />
+    }
+  ])
   return (
     <div>
-      <Header />
-      <Shop />
+      <RouterProvider router={router}/>
     </div>
   );
 }
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,23 +1,24 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import logo from "../../images/Logo.svg";
 import "./Header.css";
 
 const Header = () => {
   return (
     <nav className="header">
-      <a href="/">
+      <Link to="/">
         <img src={logo} alt="" />
-      </a>
+      </Link>
       <div>
         <input type="checkbox" id="checkbox_toggle" />
         <label htmlFor="checkbox_toggle" className="hamburger">
           &#9776;
         </label>
         <div className="menu">
-          <a href="/shop">Shop</a>
-          <a href="/orders">Orders</a>
-          <a href="/inventory">Inventory</a>
-          <a href="/about">About</a>
+          <Link to="/">Shop</Link>
+          <Link to="/orders">Orders</Link>
+          <Link to="/inventory">Inventory</Link>
+          <Link to="/about">About</Link>
         </div>
       </div>
     </nav>
